f8bf0193af9120825ee37b33d9fefb79ff799dab
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-08-07 20:36:47 +0200

    Update index.js

--- static/index.js
+++ static/index.js
@@ -2,33 +2,35 @@ function sendMessage() {
     let messageContainer = document.getElementById("messages");
     let message = document.getElementById("message").value;
 
-    let yourMessageElement = document.createElement("p");
-    yourMessageElement.innerHTML = "<b>You:</b> " + message;
-    messageContainer.appendChild(yourMessageElement);
-
-    let data = {
-        "message" : message
-    };
-    let serializedData = JSON.stringify(data);
-    document.getElementById("message").value = "";
-
-    var url = '/send-message';
-    fetch(url, {
-     method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: serializedData
-     })
-      .then(response => response.json())
-      .then(responseData => {
+    if (message.length > 0) {
         let yourMessageElement = document.createElement("p");
-        yourMessageElement.innerHTML = "<b>Bot:</b> " + responseData["reply"];
+        yourMessageElement.innerHTML = "<b>You:</b> " + message;
         messageContainer.appendChild(yourMessageElement);
-     })
-     .catch(error => {
-       console.error('Error:', error);
-      });
+
+        let data = {
+            "message" : message
+        };
+        let serializedData = JSON.stringify(data);
+        document.getElementById("message").value = "";
+
+        var url = '/send-message';
+        fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: serializedData
+        })
+        .then(response => response.json())
+        .then(responseData => {
+            let yourMessageElement = document.createElement("p");
+            yourMessageElement.innerHTML = "<b>Bot:</b> " + responseData["reply"];
+            messageContainer.appendChild(yourMessageElement);
+        })
+        .catch(error => {
+        console.error('Error:', error);
+        });
+    }
 }
 
 function checkKeyPress(event) {
